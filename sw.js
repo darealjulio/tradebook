6a34d0ccd7dccdc044090f0142c00500ed89514c
darealjulio <juliorodriguez.f0921@gmail.com>
2026-02-17 12:13:49 -0500

    Bump SW cache to v3, add features.js to assets

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tradebook-v2';
+const CACHE_NAME = 'tradebook-v3';
 const ASSETS = [
   '/',
   '/index.html',
@@ -8,6 +8,7 @@ const ASSETS = [
   '/icons/icon-192.png',
   '/auth.js',
   '/groups.js',
+      '/features.js',
   '/icons/icon-512.png',
   'https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,300;0,9..40,500;0,9..40,700;0,9..40,900;1,9..40,400&family=JetBrains+Mono:wght@500;700&display=swap'
 ];
